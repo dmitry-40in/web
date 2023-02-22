952fa3bd8dfd30b36d93bfa5feea84efd64ae030
Dmitry <dmitry_40in@mail.ru>
2023-02-22 16:31:08 +0300

    made corrections

--- task_003/1.js
+++ task_003/1.js
@@ -1,8 +1,8 @@
-const degreeC = parseFloat(prompt("Введи температуру в градуса Цельсия"));
+const degreeC = Number.parseFloat(prompt("Введи температуру в градуса Цельсия"));
 const degreeF = convertCF(degreeC);
 
 alert(`Цельсий: ${degreeC.toFixed(2)}, Фаренгейт: ${degreeF.toFixed(2)}`);
 
 function convertCF(degree) {
-    return ((9 / 5) * degree + 32);
+    return (9 / 5) * degree + 32;
 }
